d381eae97ac0cdf0b1115829e893a07b1d41352c
wnfaulkner <william@fluxrme.com>
2018-07-31 10:47:57 -0500

    Made separate function file with two functions to capitalize first letters of words in vectors.

--- FUN_FirstLetterCap.r
+++ FUN_FirstLetterCap.r
@@ -1,6 +1,15 @@
 #Capitalize the first letter of each word in a substring
-  FirstLetterCap <- function(x) {
-	s <- strsplit(x, " ")[[1]]
-	paste(toupper(substring(s, 1, 1)), tolower(substring(s, 2)),
-		  sep = "", collapse = " ")
+  
+  FirstLetterCap_OneElement <- function(x){
+    if(class(x) != "character"){print(paste("Warning: Input is of class ",toupper(class(x)),". Coercing to character vector.",sep = ""))}
+    #if(length(x) > 1){print("Warning: Input has length greater than 1. All elements will be collapsed into single element.")}
+    if(!is.null(dim(x))){print("Error: Input is not an atomic vector.")}
+    
+    s <- strsplit(as.character(x), " ")  %>% unlist
+    paste(toupper(substring(s, 1, 1)), tolower(substring(s, 2)), sep = "", collapse = " ")
+  }
+  
+  FirstLetterCap_MultElements <- function(x) {
+	  s <- strsplit(as.character(x), " ")
+	  sapply(s, FirstLetterCap_OneElement)
   }
