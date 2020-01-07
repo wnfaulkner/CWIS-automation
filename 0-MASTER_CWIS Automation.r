98129ba2bc5223d61c7e9ba927116407a4b0ff24
WNF <william@fluxrme.com>
2020-01-07 17:52:09 -0600

    Small changes to directories setup as have just reinstalled windows and changed username slightly

--- 0-MASTER_CWIS Automation.r
+++ 0-MASTER_CWIS Automation.r
@@ -19,13 +19,13 @@
   # ESTABLISH BASE DIRECTORIES
   
       # Figure out what machine code is running on
-      if(dir.exists("C:\\Users\\willi")){m900 <- TRUE}else{m900 <- FALSE}
+      if(dir.exists("C:\\Users\\WNF\\Google Drive")){m900 <- TRUE}else{m900 <- FALSE}
       
       # Set Working Directory and R Project Directory
       if(m900){  
         #M900
-          wd <- "C:\\Users\\willi\\Google Drive\\1. FLUX CONTRACTS - CURRENT\\2016-09 EXT Missouri\\3. MO GDRIVE\\8. CWIS\\2019-11_Phase 10\\"
-          rproj.dir <- "C:\\Users\\willi\\Documents\\GIT PROJECTS\\CWIS-automation\\"
+          wd <- "C:\\Users\\WNF\\Google Drive\\1. FLUX CONTRACTS - CURRENT\\2016-09 EXT Missouri\\3. MO GDRIVE\\8. CWIS\\2019-11_Phase 10\\"
+          rproj.dir <- "C:\\Users\\WNF\\Documents\\GIT PROJECTS\\CWIS-automation\\"
       }else{
         #Thinkpad T470
           wd <- "G:\\Meu Drive\\1. FLUX CONTRACTS - CURRENT\\2016-09 EXT Missouri\\3. MO GDRIVE\\8. CWIS\\2019-11_Phase 10\\"
